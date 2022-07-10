83f76a60314f69b59e17bf859ed02bdbcf78efd2
Vitor Amaral Silveira <vitor.amaral@creditas.com.br>
2022-07-10 12:23:12 -0300

    fix: use the correct type and name for direction

--- src/main/java/br/com/elo7/sonda/candidato/domain/CardinalPoints.java
+++ src/main/java/br/com/elo7/sonda/candidato/domain/CardinalPoints.java
@@ -1,16 +1,24 @@
 package br.com.elo7.sonda.candidato.domain;
 
 public enum CardinalPoints {
-    NORTH("N"), SOUTH("S"), WEST("W"), EAST("E");
+    NORTH('N'), SOUTH('S'), WEST('W'), EAST('E');
 
-    private final String command;
+    private final char direction;
 
-    CardinalPoints(String command) {
-        this.command = command;
+    CardinalPoints(char command) {
+        this.direction = command;
     }
 
-    @Override
-    public String toString() {
-        return command;
+    public char toChar() {
+        return direction;
+    }
+
+    public static CardinalPoints valueOfLabel(char label) {
+        for (CardinalPoints direction : values()) {
+            if (direction.toChar() == label) {
+                return direction;
+            }
+        }
+        return null;
     }
 }
