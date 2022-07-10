73e8d23e338d41e1cf82f01757bd43eaa64dbf30
Vitor Amaral Silveira <vitor.amaral@creditas.com.br>
2022-07-10 14:23:33 -0300

    chore: add methods to move probe on probe entity

--- src/main/java/br/com/elo7/sonda/candidato/domain/entities/ProbeEntity.java
+++ src/main/java/br/com/elo7/sonda/candidato/domain/entities/ProbeEntity.java
@@ -1,11 +1,16 @@
 package br.com.elo7.sonda.candidato.domain.entities;
 
+import br.com.elo7.sonda.candidato.domain.Direction;
 import org.hibernate.annotations.Type;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import java.util.UUID;
 
+@Entity
+@Table(name = "probes")
 public class ProbeEntity {
 
     private ProbeEntity () {}
@@ -44,4 +49,47 @@ public class ProbeEntity {
     public UUID getId() { return id; }
 
     public UUID getPlanetId() { return planetId; }
+
+    public void turnLeft() {
+        this.direction = switch (this.direction) {
+            case Direction.NORTH -> Direction.WEST;
+            case Direction.WEST -> Direction.SOUTH;
+            case Direction.SOUTH -> Direction.EAST;
+            case Direction.EAST -> Direction.NORTH;
+            default -> throw new IllegalStateException("Unexpected value: " + this.direction);
+        };
+    }
+
+    public void turnRight() {
+        this.direction = switch (this.direction) {
+            case Direction.NORTH -> Direction.EAST;
+            case Direction.EAST -> Direction.SOUTH;
+            case Direction.SOUTH -> Direction.WEST;
+            case Direction.WEST -> Direction.NORTH;
+            default -> throw new IllegalStateException("Unexpected value: " + this.direction);
+        };
+    }
+    public void moveForward(int maxX, int maxY) {
+        int newX = this.x;
+        int newY = this.y;
+
+        switch (this.direction) {
+            case Direction.NORTH -> newY = sum(this.y, maxY);
+            case Direction.WEST -> newX = sub(this.x, maxX);
+            case Direction.SOUTH -> newY = sub(this.y, maxY);
+            case Direction.EAST -> newX = sum(this.x, maxX);
+        }
+
+        this.x = newX;
+        this.y = newY;
+    }
+    private int sum(int actualValue, int maxValue) {
+        int newValue = actualValue + 1;
+        return newValue > maxValue ? 0 : newValue;
+    }
+
+    private int sub(int actualValue, int maxValue) {
+        int newValue = actualValue - 1;
+        return newValue < 0 ? maxValue : newValue;
+    }
 }
