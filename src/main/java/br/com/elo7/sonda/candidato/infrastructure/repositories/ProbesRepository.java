6fe93234cf996b051aa487d2ab6cbe068b153966
Vitor Amaral Silveira <vitor.amaral@creditas.com.br>
2022-07-10 14:22:25 -0300

    fix: use the correct annotation for JPA repository

--- src/main/java/br/com/elo7/sonda/candidato/infrastructure/repositories/ProbesRepository.java
+++ src/main/java/br/com/elo7/sonda/candidato/infrastructure/repositories/ProbesRepository.java
@@ -2,8 +2,9 @@ package br.com.elo7.sonda.candidato.infrastructure.repositories;
 
 import br.com.elo7.sonda.candidato.domain.entities.ProbeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
 import java.util.UUID;
 
-@Component
+@Repository
 public interface ProbesRepository extends JpaRepository<ProbeEntity, UUID> { }
