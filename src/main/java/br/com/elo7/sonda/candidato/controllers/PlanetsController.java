0cac359699c612e3ee52ab4846269187d93f9e25
Vitor Amaral Silveira <vitor.amaral@creditas.com.br>
2022-07-09 23:11:57 -0300

    fix: add lost annotation to map the register endpoint

--- src/main/java/br/com/elo7/sonda/candidato/controllers/PlanetsController.java
+++ src/main/java/br/com/elo7/sonda/candidato/controllers/PlanetsController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -16,7 +17,8 @@ public class PlanetsController {
     @Autowired
     PlanetsRepository planetsRepository;
 
-    public ResponseEntity<PlanetEntity> create(@RequestBody CreatePlanetRequest request) {
+    @PostMapping("/")
+    public ResponseEntity<PlanetEntity> register(@RequestBody CreatePlanetRequest request) {
         PlanetEntity planet = new PlanetEntity(request.getWidth(), request.getHeight());
         planetsRepository.save(planet);
 
