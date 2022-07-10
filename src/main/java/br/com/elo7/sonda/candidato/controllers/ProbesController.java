fdfa009fb4d67bebf2156e430633305e6737c0aa
Vitor Amaral Silveira <vitor.amaral@creditas.com.br>
2022-07-10 14:29:39 -0300

    feat: new endpoint to move probe

--- src/main/java/br/com/elo7/sonda/candidato/controllers/ProbesController.java
+++ src/main/java/br/com/elo7/sonda/candidato/controllers/ProbesController.java
@@ -3,15 +3,16 @@ package br.com.elo7.sonda.candidato.controllers;
 import br.com.elo7.sonda.candidato.domain.entities.ProbeEntity;
 import br.com.elo7.sonda.candidato.usecases.LandProbeDTO;
 import br.com.elo7.sonda.candidato.usecases.LandProbeUseCase;
+import br.com.elo7.sonda.candidato.usecases.MoveProbeDTO;
+import br.com.elo7.sonda.candidato.usecases.MoveProbeUseCase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.UUID;
 
 @Controller
 @RequestMapping("/probes")
@@ -20,18 +21,17 @@ public class ProbesController {
     @Autowired
     private LandProbeUseCase landProbeUseCase;
 
+    @Autowired
+    private MoveProbeUseCase moveProbeUseCase;
+
     // TODO() - handle de erros para http
     @PostMapping("/land")
     public ResponseEntity<ProbeEntity> land(@RequestBody LandProbeDTO request) {
         return ResponseEntity.ok(landProbeUseCase.land(request));
     }
 
-
-    /// provocações de possiveis cenários de falha
-    /// o que acontece quando dois comandos para probes diferentes a mesma posição são executados ao mesmo tempo? vão colidir?
-    /// o que acontece quando duas probes cruzam o caminho ao mesmo tempo?
-    /// identificar comandos que não fazem sentido? Por exemplo, girar sem sair do lugar
-    /// comandos muito longos podem ser um problema, Testar performance
-    /// considerar cenario de dois comandos ao mesmo tempo para uma mesma sonda?
-
+    @PutMapping("/move")
+    public ResponseEntity<ProbeEntity> move(@RequestBody MoveProbeDTO request) {
+        return new ResponseEntity<>(moveProbeUseCase.execute(request), HttpStatus.OK);
+    }
 }
