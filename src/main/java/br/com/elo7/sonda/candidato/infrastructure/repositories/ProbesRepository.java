f38719b99a124fad1f72491674e42ce7ead3834f
Vitor Amaral Silveira <vitor.amaral@creditas.com.br>
2022-07-10 12:11:50 -0300

    refact: remove unecessery method on probes repository

--- src/main/java/br/com/elo7/sonda/candidato/infrastructure/repositories/ProbesRepository.java
+++ src/main/java/br/com/elo7/sonda/candidato/infrastructure/repositories/ProbesRepository.java
@@ -2,8 +2,8 @@ package br.com.elo7.sonda.candidato.infrastructure.repositories;
 
 import br.com.elo7.sonda.candidato.domain.entities.ProbeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Component;
 import java.util.UUID;
 
-public interface ProbesRepository extends JpaRepository<ProbeEntity, UUID> {
-}
+@Component
+public interface ProbesRepository extends JpaRepository<ProbeEntity, UUID> { }
