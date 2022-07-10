9a0e3ed20195f70fd1cab1e933f7cbba9a667d00
Vitor Amaral Silveira <vitor.amaral@creditas.com.br>
2022-07-09 22:40:31 -0300

    enable JPA repositories on the application

--- src/main/java/br/com/elo7/sonda/candidato/SevenMarsApplication.java
+++ src/main/java/br/com/elo7/sonda/candidato/SevenMarsApplication.java
@@ -2,8 +2,11 @@ package br.com.elo7.sonda.candidato;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class SevenMarsApplication {
 
 	public static void main(String[] args) {
